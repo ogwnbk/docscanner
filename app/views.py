004a8312fa74b2bbe00c3a8c973e321d9b06440f
ogwnbk <ubuntu@ip-172-31-37-48.ap-northeast-1.compute.internal>
2018-12-01 17:55:49 +0000

    add template

--- app/views.py
+++ app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from . import forms
 
 def index(request):
-    return render(request,'app/index.html')
+    form = forms.UploadForm(request.GET or None)
+    if form.is_valid():
+        message = 'データ検証に成功しました'
+    else:
+        message = 'データ検証に失敗しました'
+    d = {
+        'form': form,
+        'message': message,
+    }
+    return render(request, 'app/index.html', d)
