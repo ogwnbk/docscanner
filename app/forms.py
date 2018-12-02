7812bb3c551d4ef24bc87e9fae2bbb2d93a70cbe
ogwnbk <ubuntu@ip-172-31-37-48.ap-northeast-1.compute.internal>
2018-12-02 03:05:57 +0000

    add css

--- app/forms.py
+++ app/forms.py
@@ -8,3 +8,17 @@ class UploadForm(forms.Form):
         widget=forms.TextInput()
     )
 
+    gender = forms.CharField(
+        label='Gender',
+        max_length=20,
+        required=True,
+        widget=forms.TextInput()
+    )
+
+    memo = forms.CharField(
+        label='Memo',
+        max_length=20,
+        required=True,
+        widget=forms.TextInput()
+    )
+
--- docscanner/settings.py
+++ docscanner/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
+
